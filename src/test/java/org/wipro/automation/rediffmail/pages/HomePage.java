a64d18cf583623cd2d75374eba228179ad8aeeb5
Santosh Modi <modisantosh@gmail.com>
2026-06-23 22:20:56 +0530

    this is update

---
+++ src/test/java/org/wipro/automation/rediffmail/pages/HomePage.java
@@ -0,0 +1,41 @@
+package org.wipro.automation.rediffmail.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.wipro.automation.rediffmail.utilities.ReadDataFiles;
+
+public class HomePage 
+{
+
+	WebDriver driver;
+	
+	
+	public HomePage(WebDriver driver)
+	{
+		this.driver=driver;
+		
+	}
+
+	public void enter_username_textbox(String uname) throws Exception
+	{
+		driver.findElement(By.id(ReadDataFiles.read_element_Data("login_username_id"))).sendKeys(uname);
+	}
+	
+	public void enter_password_textbox(String pass) throws Exception
+	{
+		driver.findElement(By.name(ReadDataFiles.read_element_Data("login_password_name"))).sendKeys(pass);
+	}
+	
+	public void click_checkbox_rememberme() throws Exception
+	{
+		driver.findElement(By.cssSelector(ReadDataFiles.read_element_Data("login_checkbox_rememberme_css"))).click();
+	}
+	
+	public void click_loginbttn() throws Exception
+	{
+		driver.findElement(By.className(ReadDataFiles.read_element_Data("login_loginbttn_class"))).click();
+	}
+	
+}
